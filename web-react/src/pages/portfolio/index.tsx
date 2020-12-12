e0bd6cd2d22b6fea960f57d487e04babe40781bd
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-12-12 13:33:02 +0800

    refactor portfolio and about pages

--- web-react/src/pages/portfolio/index.tsx
+++ web-react/src/pages/portfolio/index.tsx
@@ -1,36 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MediaTextSection from '../../components/layout/media-text-section';
 import productivityImage from './images/productivity.png';
 import gamesImage from './images/games.png';
 import './portfolio.css';
 
 const Portfolio = () => (
-  <main>
-    <div className="portfolio__row">
-      <figure className="portfolio__contentImage__container">
-        <img src={productivityImage} alt="Productivity" className="portfolio__contentImage__image" />
-      </figure>
-      <div className="portfolio__contentText__container--right">
-        <h1 className="portfolio__contentText__heading">Productivity</h1>
-        <div className="portfolio__contentText__content">
-          <p>Tools that can boost your productivity, no matter what you do.</p>
-          <Link to="/portfolio/productivity" className="portfolio__contentText__content--seeAll">See all</Link>
-        </div>
-      </div>
-    </div>
-    <div className="portfolio__row">
-      <div className="portfolio__contentText__container--left">
-        <h1 className="portfolio__contentText__heading">Fun &amp; Games</h1>
-        <div className="portfolio__contentText__content">
-          <p>Fight boredom with some fun games to play with your friends.</p>
-          <Link to="/portfolio/fun-games" className="portfolio__contentText__content--seeAll">See all</Link>
-        </div>
-      </div>
-      <figure className="portfolio__contentImage__container">
-        <img src={gamesImage} alt="Fun and Games" className="portfolio__contentImage__image" />
-      </figure>
-    </div>
-  </main>
+    <main>
+        <MediaTextSection heading="Productivity" imageSide="right" image={{ src: productivityImage, alt: 'Productivity' }}>
+            <p>Tools that can boost your productivity, no matter what you do.</p>
+            <Link to="/portfolio/productivity" className="portfolio__contentText__content--seeAll">See all</Link>
+        </MediaTextSection>
+        <MediaTextSection heading="Fun &amp; Games" imageSide="left" image={{ src: gamesImage, alt: 'Fun and Games' }}>
+            <p>Fight boredom with some fun games to play with your friends.</p>
+            <Link to="/portfolio/fun-games" className="portfolio__contentText__content--seeAll">See all</Link>
+        </MediaTextSection>
+    </main>
 );
 
 export default Portfolio;
