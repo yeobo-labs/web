c23c7cd89a508443a2b39137629f6b5362ec7eeb
Louise Ann Apostol <louiseann.apostol@gmail.com>
2020-12-12 16:14:34 +0800

    MediaTextSection videos (#11)
    * video props
    
    * allow video prop
    
    * use video in /portfolio
    
    * lint

--- web-react/src/components/layout/media-text-section.tsx
+++ web-react/src/components/layout/media-text-section.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { ImageProps } from '../../types/image';
+import { VideoProps } from '../../types/video';
 import './layout.css';
 
-type MediaTextSectionProps = {
-    image: ImageProps;
-    heading: string;
-    children: React.ReactNode;
-    imageSide: 'left' | 'right';
+type MediaTextSectionMediaProps = {
+    image?: ImageProps;
+    video?: VideoProps;
 }
 
-const renderImage = (image: ImageProps) => (
+type MediaTextSectionProps = MediaTextSectionMediaProps & {
+    heading: string;
+    children: React.ReactNode;
+    mediaSide: 'left' | 'right';
+}
+
+const renderMedia = ({video, image}: MediaTextSectionMediaProps) => (
     <figure className="mediaTextSection__contentImage__container">
-        <img {...image} alt={image.alt || ''} className="mediaTextSection__contentImage__image" />
+        {video ? <video {...video} /> : (image ? <img {...image} alt={image.alt || ''} className="mediaTextSection__contentImage__image" /> : <div />)}
     </figure>
 );
 
-const renderContent = (props: Omit<MediaTextSectionProps, 'image'>) => (
-    <div className={`mediaTextSection__contentText__container--${props.imageSide}`}>
+const renderContent = (props: Omit<MediaTextSectionProps, 'image' | 'video'>) => (
+    <div className={`mediaTextSection__contentText__container--${props.mediaSide}`}>
         <h1 className="mediaTextSection__contentText__heading">{props.heading}</h1>
         <div className="mediaTextSection__contentText__content">
             {props.children}
@@ -25,11 +30,11 @@ const renderContent = (props: Omit<MediaTextSectionProps, 'image'>) => (
 );
 
 const MediaTextSection = (props: MediaTextSectionProps) => {
-    const mediaContent = renderImage(props.image);
+    const mediaContent = renderMedia(props);
     const textContent = renderContent(props);
 
-    const first = props.imageSide === 'left' ? mediaContent : textContent;
-    const second = props.imageSide === 'left' ? textContent : mediaContent;
+    const first = props.mediaSide === 'left' ? mediaContent : textContent;
+    const second = props.mediaSide === 'left' ? textContent : mediaContent;
 
     return (<div className="mediaTextSection__row">
         {first}
--- web-react/src/pages/portfolio/index.tsx
+++ web-react/src/pages/portfolio/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import MediaTextSection from '../../components/layout/media-text-section';
-import productivityImage from './images/productivity.png';
-import gamesImage from './images/games.png';
+import gamesVideo from './videos/games.mp4';
+import productivityVideo from './videos/productivity.mp4';
 import './portfolio.css';
 
 const Portfolio = () => (
     <main>
-        <MediaTextSection heading="Productivity" imageSide="right" image={{ src: productivityImage, alt: 'Productivity' }}>
+        <MediaTextSection
+            heading="Productivity"
+            mediaSide="right"
+            video={{ src: productivityVideo, autoPlay: true, loop: true }}>
             <p>Tools that can boost your productivity, no matter what you do.</p>
             <Link to="/portfolio/productivity" className="portfolio__contentText__content--seeAll">See all</Link>
         </MediaTextSection>
-        <MediaTextSection heading="Fun &amp; Games" imageSide="left" image={{ src: gamesImage, alt: 'Fun and Games' }}>
+        <MediaTextSection
+            heading="Fun &amp; Games"
+            mediaSide="left"
+            video={{ src: gamesVideo, autoPlay: true, loop: true }}>
             <p>Fight boredom with some fun games to play with your friends.</p>
             <Link to="/portfolio/fun-games" className="portfolio__contentText__content--seeAll">See all</Link>
         </MediaTextSection>
